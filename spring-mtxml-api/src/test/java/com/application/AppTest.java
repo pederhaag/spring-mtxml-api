7a351c143d6d88e2152d20dc5a325706b6420019
pederhaag <peder.haagensli@gmail.com>
2021-10-29 09:14:55 +0200

    cleanup

--- spring-mtxml-api/src/test/java/com/application/AppTest.java
+++ spring-mtxml-api/src/test/java/com/application/AppTest.java
@@ -1,10 +1,10 @@
-package com.example.accessingdatamysql;
+package com.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccessingDataMysqlApplicationTests {
+class AppTest {
 
 	@Test
 	void contextLoads() {
